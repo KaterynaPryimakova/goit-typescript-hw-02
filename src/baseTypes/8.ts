b9e406fb85808dc1b29d00a6e7bafd98275baff3
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-02-06 15:09:47 +0200

    new: baswTypes\6-9task

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -2,7 +2,7 @@
   Створіть тип "Gender", використовуючи union type, 
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
+type Gender = "male" | "femail";
+const myGender: Gender = "femail";
 
-const myGender;
-
-export {};
+export { myGender };
